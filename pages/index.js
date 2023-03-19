089421c0c39ca3013b028a8b4e9bebe2c7953865
drormaman <drormaman@gmail.com>
2023-03-19 19:30:55 +0200

    tailwind reset and postcss

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import '@/styles/Home.module.css'
+// import '@/tailwind.config'
 
 export default function Home() {
   return (
@@ -10,7 +10,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
       <main>
+        <section className="bg-blue-700 h-screen">
+        hello
+        </section>
 
       </main>
     </>
